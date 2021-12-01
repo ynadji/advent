477d805a3d36bae21f180e79128516c8a672f4da
Yacin Nadji <ynadji@gmail.com>
2021-12-01 10:54:27 -0500

    factor out utils

---
+++ 2021/prolog/utils.pl
@@ -0,0 +1,21 @@
+use_module(library(pio)).
+
+% Voodoo I don't understand yet.
+lines([])           --> call(eos), !.
+lines([Line|Lines]) --> line(Line), lines(Lines).
+
+eos([], []).
+
+line([])     --> ( "\n" ; call(eos) ), !.
+line([L|Ls]) -->
+    [L], line(Ls).
+
+lines_to_numbers([], []).
+lines_to_numbers([HL|Ls], [HN|Ns]) :-
+    atom_chars(X, HL),
+    atom_number(X, HN),
+    lines_to_numbers(Ls, Ns).
+
+numbers_from_file(Ns, File) :-
+    phrase_from_file(lines(Ls), File),
+    lines_to_numbers(Ls, Ns).
