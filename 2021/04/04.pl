100685df0558c86e381eb6f8eca537d9678478c6
Yacin Nadji <ynadji@gmail.com>
2021-12-04 16:27:38 -0500

    uhh it works?? no clue why it suddenly started going fast /shrugs

--- 2021/04/04.pl
+++ 2021/04/04.pl
@@ -1,5 +1,6 @@
 :- use_module(library(clpfd)).
 :- use_module(library(csv)).
+:- use_module(library(statistics)).
 
 ['../prolog/utils'].
 
@@ -88,7 +89,7 @@ if it finishes by the time I get back from my bike ride.
 
 The rough approach is as follows:
 * For each subset of Nums,
-* Exclude all boards that succeeds,
+* Exclude all boards that succeed,
 * Until there is exactly one board (the last board to win),
 * Find the solution/score for the last board.
 
@@ -100,17 +101,38 @@ way, we will probably succeed more quickly.
 * The set stuff is very inefficient according to the docs.
 
 reverse([1,2,3,4,5,6,7], L), append(Try, Other, L), reverse(Try, TryR), reverse(Other, OtherR).
+
+* can i do an include() to identify which boards already pass, so i can ignore them on future runs?
+* https://www.swi-prolog.org/pldoc/doc/_SWI_/library/ordsets.pl
+  * made some changes. everything is right _except_ the score...
+
+uhh all of a sudden it's fast now?? wtf?? so things were slow. i implemented the
+reversed_append where instead of checking Nums[:1], Nums[:2], ... i check Nums[:-1],
+Nums[:-2], ... since i figured it was more likely the last solvable one would use closer
+to most of the Nums. still slow af. tried to use ordsets, but was getting incorrect
+answers so i stashed those changes. added the profiler, profiled the small set,
+all normal. profiled the long running solution, immediately pooped out an answer!
+WHAT??
+
+i hate not understanding things D:<
 */
 solution_04_02__(Nums, Boards, LastBoard) :-
-    append(Try, _, Nums),
+    reverse(Nums, NumsR),
+    append(_, TryR, NumsR),
+    reverse(TryR, Try),
     exclude(new_board(Try, _), Boards, Remaining),
     length(Remaining, 1),
     nth1(1, Remaining, LastBoard).
 
+% for example
+% solution_04_02(Score).
+% Score = 1924
+% solution_04_02(Score).
+% Score = 6804
 solution_04_02(Score) :-
-    %read_input(Nums, Boards),
-    Nums = [7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1],
-    Boards = [[[22,13,17,11, 0], [8, 2,23, 4,24], [21, 9,14,16, 7], [6,10, 3,18, 5], [1,12,20,15,19]], [[14, 21, 17, 24,  4], [10, 16, 15,  9, 19], [18,  8, 23, 26, 20], [22, 11, 13,  6,  5], [2,  0, 12,  3,  7]], [[3, 15,  0,  2, 22], [9, 18, 13, 17,  5], [19,  8,  7, 25, 23], [20, 11, 10, 24,  4], [14, 21, 16, 12,  6]]],
+    read_input(Nums, Boards),
+    %Nums = [7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1],
+    %Boards = [[[22,13,17,11, 0], [8, 2,23, 4,24], [21, 9,14,16, 7], [6,10, 3,18, 5], [1,12,20,15,19]], [[14, 21, 17, 24,  4], [10, 16, 15,  9, 19], [18,  8, 23, 26, 20], [22, 11, 13,  6,  5], [2,  0, 12,  3,  7]], [[3, 15,  0,  2, 22], [9, 18, 13, 17,  5], [19,  8,  7, 25, 23], [20, 11, 10, 24,  4], [14, 21, 16, 12,  6]]],
     solution_04_02__(Nums, Boards, LastBoard),
     append(Try, _, Nums),
     board(Try, LastBoard, Score).
