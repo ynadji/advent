a6454bda234fd68a72e9e4a56c892905db265485
Yacin Nadji <ynadji@gmail.com>
2021-12-04 13:53:49 -0500

    solution 04.02 works, but is way too slow

--- 2021/04/04.pl
+++ 2021/04/04.pl
@@ -12,8 +12,14 @@ read_input(Nums, Boards) :-
     split_list(BoardRows, 5, Boards).
 
 % [7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1]
+%
+% [7,4,9,5,11,17,23,2,0,14,21,24,10,16]
 % [[22,13,17,11, 0], [8, 2,23, 4,24], [21, 9,14,16, 7], [6,10, 3,18, 5], [1,12,20,15,19]]
+%
+% [7,4,9,5,11,17,23,2,0,14,21,24]
 % [[14, 21, 17, 24,  4], [10, 16, 15,  9, 19], [18,  8, 23, 26, 20], [22, 11, 13,  6,  5], [2,  0, 12,  3,  7]]
+%
+% [7,4,9,5,11,17,23,2,0,14,21,24,10,16,13]
 % [[3, 15,  0,  2, 22], [9, 18, 13, 17,  5], [19,  8,  7, 25, 23], [20, 11, 10, 24,  4], [14, 21, 16, 12,  6]]
 % [[[22,13,17,11, 0], [8, 2,23, 4,24], [21, 9,14,16, 7], [6,10, 3,18, 5], [1,12,20,15,19]], [[14, 21, 17, 24,  4], [10, 16, 15,  9, 19], [18,  8, 23, 26, 20], [22, 11, 13,  6,  5], [2,  0, 12,  3,  7]], [[3, 15,  0,  2, 22], [9, 18, 13, 17,  5], [19,  8,  7, 25, 23], [20, 11, 10, 24,  4], [14, 21, 16, 12,  6]]]
     
@@ -73,3 +79,38 @@ solution_04_01__(Nums, Boards, Score) :-
     % I think this is the trick!
     append(Try, _, Nums),
     solution_04_01_(Try, Boards, Score).
+
+new_board(Nums, _, Board) :- board(Nums, Board, _).
+
+/*
+Very inefficient solution, but it was correct for the short example. We'll see
+if it finishes by the time I get back from my bike ride.
+
+The rough approach is as follows:
+* For each subset of Nums,
+* Exclude all boards that succeeds,
+* Until there is exactly one board (the last board to win),
+* Find the solution/score for the last board.
+
+It's slow af for a couple of reasons:
+* We recompute Try when we might as well at least start from the point where it
+was the only unsuccessful goal.
+* We try increasing, rather than decreasing, subsets of Nums. If we go the other
+way, we will probably succeed more quickly.
+* The set stuff is very inefficient according to the docs.
+
+reverse([1,2,3,4,5,6,7], L), append(Try, Other, L), reverse(Try, TryR), reverse(Other, OtherR).
+*/
+solution_04_02__(Nums, Boards, LastBoard) :-
+    append(Try, _, Nums),
+    exclude(new_board(Try, _), Boards, Remaining),
+    length(Remaining, 1),
+    nth1(1, Remaining, LastBoard).
+
+solution_04_02(Score) :-
+    %read_input(Nums, Boards),
+    Nums = [7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1],
+    Boards = [[[22,13,17,11, 0], [8, 2,23, 4,24], [21, 9,14,16, 7], [6,10, 3,18, 5], [1,12,20,15,19]], [[14, 21, 17, 24,  4], [10, 16, 15,  9, 19], [18,  8, 23, 26, 20], [22, 11, 13,  6,  5], [2,  0, 12,  3,  7]], [[3, 15,  0,  2, 22], [9, 18, 13, 17,  5], [19,  8,  7, 25, 23], [20, 11, 10, 24,  4], [14, 21, 16, 12,  6]]],
+    solution_04_02__(Nums, Boards, LastBoard),
+    append(Try, _, Nums),
+    board(Try, LastBoard, Score).
