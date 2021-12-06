aa959ed1eea91e74700efed14e6ef745117b3c36
Yacin Nadji <ynadji@gmail.com>
2021-12-05 20:21:20 -0500

    refactor

--- 2021/05/05.pl
+++ 2021/05/05.pl
@@ -35,45 +35,32 @@ all_between(X1, X2, Xs) :-
     reverse(Xs_, Xs)
     ).
 
-hov_line([X, Y, X, Y]) :- assert_points([X, Y]).
-hov_line([X1,Y, X2,Y]) :-
+draw_line([X, Y, X, Y]) :- assert_points([X, Y]).
+draw_line([X1,Y, X2,Y]) :-
     all_between(X1, X2, Xs),
     assert_points([Xs, Y]).
-hov_line([X, Y1, X, Y2]) :-
+draw_line([X, Y1, X, Y2]) :-
     all_between(Y1, Y2, Ys),
     assert_points([X, Ys]).
+draw_line([X1, Y1, X2, Y2]) :-
+    all_between(X1, X2, Xs),
+    all_between(Y1, Y2, Ys),
+    assert_points([Xs, Ys]).
 
-hov_lines([]).
-hov_lines([Points|Rest]) :-
-    hov_line(Points),
-    hov_lines(Rest).
+draw_lines([]).
+draw_lines([Points|Rest]) :-
+    draw_line(Points),
+    draw_lines(Rest).
 
 solution_05_01(Count) :-
     slurp(Lines, '/home/yacin/code/advent/2021/05/input.txt'),
     maplist(split_points, Lines, PointsList),
     include(horizontal_or_vertical, PointsList, HOVPoints),
-    hov_lines(HOVPoints),
+    draw_lines(HOVPoints),
     aggregate_all(count, overlaps, Count).
-
-hovod_line([X, Y, X, Y]) :- assert_points([X, Y]).
-hovod_line([X1,Y, X2,Y]) :-
-    all_between(X1, X2, Xs),
-    assert_points([Xs, Y]).
-hovod_line([X, Y1, X, Y2]) :-
-    all_between(Y1, Y2, Ys),
-    assert_points([X, Ys]).
-hovod_line([X1, Y1, X2, Y2]) :-
-    all_between(X1, X2, Xs),
-    all_between(Y1, Y2, Ys),
-    assert_points([Xs, Ys]).
-
-hovod_lines([]).
-hovod_lines([Points|Rest]) :-
-    hovod_line(Points),
-    hovod_lines(Rest).
 
 solution_05_02(Count) :-
     slurp(Lines, '/home/yacin/code/advent/2021/05/input.txt'),
     maplist(split_points, Lines, PointsList),
-    hovod_lines(PointsList),
+    draw_lines(PointsList),
     aggregate_all(count, overlaps, Count).
