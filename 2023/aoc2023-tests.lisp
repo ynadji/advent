35047cb8ff2b3ba79ef97f8baed4ede697d7fb89
Yacin Nadji <ynadji@gmail.com>
2024-06-26 16:54:35 +0200

    prob not any better but oh well

--- 2023/aoc2023-tests.lisp
+++ 2023/aoc2023-tests.lisp
@@ -1,19 +1,20 @@
-(use-package '(:fiveam))
+(ql:quickload :fiveam)
+;;(use-package '(:fiveam))
 
-(def-suite aoc2023)
-(in-suite aoc2023)
+(5am:def-suite aoc2023)
+(5am:in-suite aoc2023)
 
-(test test-day-01
+(5am:test test-day-01
   (multiple-value-bind (res1 res2) (day-01)
-    (is (= res1 55447))
-    (is (= res2 54706))))
+    (5am:is (= res1 55447))
+    (5am:is (= res2 54706))))
 
-(test test-day-02
+(5am:test test-day-02
   (multiple-value-bind (res1 res2) (day-02)
-    (is (= res1 2795))
-    (is (= res2 75561))))
+    (5am:is (= res1 2795))
+    (5am:is (= res2 75561))))
 
-(test test-day-03
+(5am:test test-day-03
   (multiple-value-bind (res1 res2) (day-03)
-    (is (= res1 551094))
-    (is (= res2 80179647))))
+    (5am:is (= res1 551094))
+    (5am:is (= res2 80179647))))
--- 2023/aoc2023.asd
+++ 2023/aoc2023.asd
@@ -9,9 +9,9 @@
                (:file "03"))
   :in-order-to ((test-op (test-op :aoc2023/tests))))
 
-(asdf:defsystem "aoc2023/tests"
+(asdf:defsystem :aoc2023/tests
   :author "Yacin Nadji <ynadji@gmail.com>"
   :license "MIT"
   :depends-on ("aoc2023" "fiveam")
   :components ((:file "aoc2023-tests"))
-  :perform (test-op (o c) (symbol-call :fiveam '#:run! :aoc2023)))
+  :perform (test-op (o c) (symbol-call :fiveam '#:run! 'aoc2023)))
