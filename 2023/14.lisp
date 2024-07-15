d235ed5d40aa98368b6731ce1575a44756114321
Yacin Nadji <ynadji@gmail.com>
2024-07-15 21:52:30 +0200

    finish day 14

--- 2023/14.lisp
+++ 2023/14.lisp
@@ -1,8 +1,50 @@
 (in-package :aoc2023)
 
-(defun day-14-part-1 (input-file) (progn input-file -1))
+(defun roll-right (chars &optional (acc nil) (rocks nil) (dots nil))
+  "Recurse over CHARS until we hit a #\#, then CONS the rolled order of that
+group. Do one more CONS when we run out of CHARS and REVERSE FLATTEN."
+  (if-let ((c (first chars)))
+    (case c
+      (#\. (roll-right (rest chars) acc rocks (cons c dots)))
+      (#\O (roll-right (rest chars) acc (cons c rocks) dots))
+      (#\# (roll-right (rest chars) (cons (append dots rocks (list #\#)) acc) nil nil)))
+    (alexandria:flatten (reverse (cons (append dots rocks) acc)))))
 
-(defun day-14-part-2 (input-file) (progn input-file -1))
+(defun score-chars (chars)
+  (loop for i from 1 for c in chars sum (if (char= c #\O) i 0)))
+
+(defun score-row (row) (-> row (coerce 'list) roll-right score-chars))
+
+(defun day-14-part-1 (input-file)
+  (loop for row in (->> input-file uiop:read-file-lines (mapcar #'string-to-chars) rotate-90-clockwise)
+        sum (score-row row)))
+
+(defun cycle (list-of-chars)
+  (->> list-of-chars
+    rotate-90-clockwise ; north
+    (mapcar #'roll-right)
+    rotate-90-clockwise ; west
+    (mapcar #'roll-right)
+    rotate-90-clockwise ; south
+    (mapcar #'roll-right)
+    rotate-90-clockwise ; east
+    (mapcar #'roll-right)))
+
+(defun score-rows (rows)
+  (loop for row in rows sum (score-chars row)))
+
+(defun day-14-part-2 (input-file)
+  (let ((northern-chars (->> input-file uiop:read-file-lines (mapcar #'string-to-chars)))
+        (cache (make-hash-table :test #'equal :size 1024)))
+    (loop for cycles from 0
+          do (if-let ((prev-cycle (gethash northern-chars cache)))
+               (progn
+                 (loop repeat (mod (- 1000000000 cycles)
+                                   (- cycles prev-cycle))
+                       do (setf northern-chars (cycle northern-chars)))
+                 (return (score-rows (rotate-90-clockwise northern-chars))))
+               (progn (setf (gethash northern-chars cache) cycles)
+                      (setf northern-chars (cycle northern-chars)))))))
 
 (defun day-14 ()
   (let ((f (fetch-day-input-file 2023 14)))
