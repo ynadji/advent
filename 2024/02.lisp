176631ff17843f30624fa3c6c7baca470cf7ec68
Yacin Nadji <ynadji@gmail.com>
2024-12-02 00:49:53 -0500

    day 2

--- 2024/02.lisp
+++ 2024/02.lisp
@@ -1,10 +1,23 @@
 (in-package :aoc2024)
 
-(defun day-02-part-1 (input-file) (progn input-file -1))
+(defun safe-report? (levels)
+  (and (or (apply #'< levels)
+           (apply #'> levels))
+       (every (lambda (x) (<= x 3))
+              (loop for (x y) on levels by #'cdr when y collect (abs (- x y))))))
 
-(defun day-02-part-2 (input-file) (progn input-file -1))
+(defun safe-report-with-tolerance? (levels)
+  (or (safe-report? levels)
+      (loop for i from 0 for x in levels
+            when (safe-report? (remove x levels :start i :count 1))
+              return t)))
+
+(defun count-safe-reports (input-file report-func)
+  (loop for line in (uiop:read-file-lines input-file)
+        for levels = (mapcar #'parse-integer (str:split " " line))
+        count (funcall report-func levels)))
 
 (defun day-02 ()
   (let ((f (fetch-day-input-file 2024 2)))
-    (values (day-02-part-1 f)
-            (day-02-part-2 f))))
+    (values (count-safe-reports f #'safe-report?)
+            (count-safe-reports f #'safe-report-with-tolerance?))))
