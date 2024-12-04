5e8cfa7508eea3478cafc8f7a6a55791d3d0843c
Yacin Nadji <ynadji@gmail.com>
2024-12-04 01:04:50 -0500

    day 4

--- 2024/04.lisp
+++ 2024/04.lisp
@@ -1,8 +1,65 @@
 (in-package :aoc2024)
 
-(defun day-04-part-1 (input-file) (progn input-file -1))
+(defparameter test-input "MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX")
 
-(defun day-04-part-2 (input-file) (progn input-file -1))
+(defun count-xmas-samx (string)
+  (+ (length (cl-ppcre:all-matches-as-strings "XMAS" string))
+     (length (cl-ppcre:all-matches-as-strings "SAMX" string))))
+
+(defun get-diagonals (seqs &optional (n 1))
+  (if (null (ax:lastcar seqs))
+      nil
+      (let ((relevant (subseq seqs 0 n)))
+        (cons (remove nil (mapcar #'first relevant))
+              (get-diagonals (append (mapcar #'rest relevant)
+                                     (subseq seqs n))
+                             (min (1+ n) (length seqs)))))))
+
+(defun day-04-part-1 (input-file)
+  (let ((lines (uiop:read-file-lines input-file)))
+    (+ (apply #'+ (mapcar #'count-xmas-samx lines))
+       (apply #'+ (mapcar #'count-xmas-samx (transpose lines)))
+       (apply #'+ (mapcar #'count-xmas-samx (mapcar #'chars-to-string (get-diagonals (mapcar #'string-to-chars lines)))))
+       (apply #'+ (mapcar #'count-xmas-samx (mapcar #'chars-to-string (get-diagonals (mapcar #'string-to-chars (rotate-90-clockwise lines)))))))))
+
+(defun read-grid (input-file)
+  (let* ((lines (uiop:read-file-lines input-file))
+         (rows (length lines))
+         (cols (length (first lines)))
+         (grid (make-array (list rows cols) :element-type 'base-char)))
+    (loop for row in lines for i from 0 do
+      (loop for x across row for j from 0 do
+        (setf (aref grid i j) x)))
+    grid))
+
+(defun get-cross (grid i j)
+  (values (aref grid i j)
+          (loop for (i . j) in (2d-neighbors grid (cons i j) :wanted-directions *inter-cardinals*)
+                collect (aref grid i j))))
+
+(defun x-mas (grid i j)
+  (and (eq #\A (aref grid i j))
+       (let ((cross (loop for (i . j) in (2d-neighbors grid (cons i j) :wanted-directions *inter-cardinals*)
+                          collect (aref grid i j))))
+         (or (equal cross '(#\S #\S #\M #\M))
+             (equal cross '(#\M #\M #\S #\S))
+             (equal cross '(#\S #\M #\M #\S))
+             (equal cross '(#\M #\S #\S #\M))))))
+
+(defun day-04-part-2 (input-file)
+  (let ((grid (read-grid input-file)))
+    (loop for i below (array-dimension grid 0)
+          sum (loop for j below (array-dimension grid 1)
+                    count (x-mas grid i j)))))
 
 (defun day-04 ()
   (let ((f (fetch-day-input-file 2024 4)))
