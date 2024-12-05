21882c39a48ad76ea6613b51cbd9ef6ed3f7fb22
Yacin Nadji <ynadji@gmail.com>
2024-12-05 11:18:14 -0500

    day 5

--- 2024/05.lisp
+++ 2024/05.lisp
@@ -1,8 +1,67 @@
 (in-package :aoc2024)
 
-(defun day-05-part-1 (input-file) (progn input-file -1))
+(defparameter test-input "47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
 
-(defun day-05-part-2 (input-file) (progn input-file -1))
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47")
+
+(defvar *ht* nil)
+
+(defun sorter (x y)
+  (if y (member y (gethash x *ht*)) t))
+
+(defun sorted? (list)
+  (loop for (x y) on list always (sorter x y)))
+
+(defun middle (list)
+  (nth (floor (/ (length list) 2)) list))
+
+(defun parse-05 (input-file)
+  (let ((lines (uiop:read-file-lines input-file))
+        (ht (make-hash-table))
+        updates)
+    (loop for line in lines with order? = t do
+      (cond ((string= line "") (setf order? nil))
+            (order? (cl-ppcre:register-groups-bind ((#'parse-integer x y)) ("(\\d+)\\|(\\d+)" line)
+                      (push y (gethash x ht))))
+            (t (push (mapcar #'parse-integer (str:split "," line)) updates))))
+    (values ht updates)))
+
+(defun day-05-part-1 (input-file)
+  (multiple-value-bind (ht updates) (parse-05 input-file)
+    (let ((*ht* ht))
+      (loop for update in updates when (sorted? update) sum (middle update)))))
+
+(defun day-05-part-2 (input-file)
+  (multiple-value-bind (ht updates) (parse-05 input-file)
+    (let ((*ht* ht))
+      (let ((ooo-updates (loop for update in updates unless (sorted? update) collect update)))
+        (loop for update in ooo-updates for sorted-update = (sort (copy-list update) #'sorter)
+              sum (middle sorted-update))))))
 
 (defun day-05 ()
   (let ((f (fetch-day-input-file 2024 5)))
