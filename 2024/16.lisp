0090cb7bd42544309fe32fb4125193893ab02f0a
Yacin Nadji <ynadji@gmail.com>
2024-12-16 17:51:54 -0500

    part 1 works

--- 2024/16.lisp
+++ 2024/16.lisp
@@ -42,62 +42,59 @@
   (format stream "#<Path to ~a cost ~,1f>"
           (path-state path) (path-total-cost path)))
 
-(defun solve-maze (paths maze goal? cost-fn cost-left-fn &optional (state= #'equal) old-paths)
-  (cond ((null paths) (error "ruh roh"))
-        ((funcall goal? (path-state (first paths)))
-         (values (first paths) paths))
-        (t (let* ((path (pop paths))
-                  (state (path-state path))
-                  (direction (path-direction path)))
-             (setf old-paths (insert-path path old-paths))
-             (multiple-value-bind (new-states new-dirs)
-                 (2d-neighbors maze state :reachable? (lambda (m pos dir) (declare (ignorable dir))
-                                                                (char/= #\# (paref m pos))))
-               ;; TODO make sure we aren't retracting steps (e.g., (cons direction pos) is new)
-               (loop for new-state in new-states for new-dir in new-dirs
-                     for cost = (+ (path-cost-so-far path)
-                                   (cond ((eq new-dir direction) 1)
-                                         ((eq (opposite-direction direction) new-dir) 2001)
-                                         (t 1001)))
-                     for path2 = (make-path :state new-state :direction new-dir :previous path :cost-so-far cost)
-                     do (let ((old nil))
-                          (cond
-                            ((setf old (find-path new-state paths state=))
-                             (when (better-path path2 old)
-                               (setf paths (insert-path
-                                            path2 (delete old paths)))))
-                            ((setf old (find-path new-state old-paths state=))
-                             (when (better-path path2 old)
-                               (setf paths (insert-path path2 paths))
-                               (setf old-paths (delete old old-paths))))
-                            (t (setf paths (insert-path path2 paths))))))
-               (solve-maze paths maze goal? cost-fn cost-left-fn state= old-paths))))))
+(defun initialize-dist (maze)
+  (let ((dist (make-hash-table :test #'equal :size (* 4 (array-total-size maze)))))
+    (loop for i below (array-dimension maze 0) do
+      (loop for j below (array-dimension maze 1) do
+        (loop for dir in *cardinals* do
+          (unless (char= #\# (aref maze i j))
+           (setf (gethash (cons dir (cons i j)) dist) most-positive-fixnum)))))
+    dist))
 
+(defun min-score-state (states dist)
+  (let ((min-state (first states))
+        (min-score most-positive-fixnum))
+    (loop for state in states
+          for state-score = (gethash state dist)
+          when (< state-score min-score)
+            do (setf min-state state min-score state-score))
+    ;;(format t "min-state: ~a~%" min-state)
+    (values min-state min-score)))
 
-(defun find-path (state paths state=)
-  "Find the path with this state among a list of paths."
-  (find state paths :key #'path-state :test state=))
+(defun dijkstra (start maze)
+  (let* ((dist (initialize-dist maze))
+         (prev (make-hash-table :test #'equal))
+         (states (ax:hash-table-keys dist)))
+    (setf (gethash start dist) 0)
+    (loop while states
+          for state = (min-score-state states dist)
+          for dir = (car state)
+          for (new-states new-dirs) = (multiple-value-list (2d-neighbors maze (cdr state)
+                                                                         :reachable? (lambda (m pos dir)
+                                                                                       (declare (ignorable dir))
+                                                                                       (char/= #\# (paref m pos)))))
+          do ;;(format t "state: ~a, dir: ~a~%" state dir)
+             ;;(format t "new-states: ~a, new-dirs: ~a~%" new-states new-dirs)
+             ;;(format t "new-new-states: ~a~%" (mapcar #'cons new-dirs new-states))
+             ;;(format t "new-new-states-have: ~a~%" (intersection states (mapcar #'cons new-dirs new-states) :test #'equal))
+             (loop for new-state in (intersection states (mapcar #'cons new-dirs new-states) :test #'equal)
+                   for new-dir = (car new-state)
+                   for new-cost = (cond ((eq new-dir dir) 1)
+                                        ((eq (opposite-direction dir) new-dir) 2001)
+                                        (t 1001))
+                   do ;;(format t "    new-state: ~a, new-dir: ~a, cost: ~a~%" new-state new-dir new-cost)
+                      (let ((alt (+ (gethash state dist) new-cost)))
+                        (when (< alt (gethash new-state dist))
+                          (setf (gethash new-state dist) alt)
+                          (setf (gethash new-state prev) state))))
+             (setf states (remove state states :test #'equal)))
+    (values dist prev)))
 
-(defun better-path (path1 path2)
-  "Is path1 cheaper than path2?"
-  (< (path-total-cost path1) (path-total-cost path2)))
-
-(defun insert-path (path paths)
-  "Put path into the right position, sorted by total cost."
-  ;; MERGE is a built-in function
-  (merge 'list (list path) paths #'< :key #'path-total-cost))
-
-(defun path-states (path)
-  "Collect the states along this path."
-  (if (null path)
-      nil
-      (cons (path-state path)
-            (path-states (path-previous path)))))
-
-(defun day-16-part-1 (input-file) (multiple-value-bind (maze starts) (read-grid input-file :starts? (lambda (c) (member c '(#\S #\E))))
-    (destructuring-bind (start goal) starts
-      (let ((path (make-path :state start :direction :east)))
-        (solve-maze (list path) maze (lambda (s) (equal s goal)) nil nil)))))
+(defun day-16-part-1 (input-file)
+  (multiple-value-bind (maze starts) (read-grid input-file :starts? (lambda (c) (member c '(#\S #\E))))
+    (let ((start (cons :east (first starts))))
+      (multiple-value-bind (dist prev) (dijkstra start maze)
+        (nth-value 1 (min-score-state (loop for dir in *cardinals* collect (cons dir (second starts))) dist))))))
 
 (defun day-16-part-2 (input-file)
   (progn input-file -1))
