c9796a35bf4671577181cdf5d71b1a3f9eb73813
Yacin Nadji <ynadji@gmail.com>
2024-12-17 10:08:12 -0500

    progress but not yet

--- 2024/17.lisp
+++ 2024/17.lisp
@@ -1,16 +1,51 @@
 (in-package :aoc2024)
 
-(defparameter test-input "Register A: 729
+(defparameter test-input-1 "Register A: 729
 Register B: 0
 Register C: 0
 
 Program: 0,1,5,4,3,0")
+
+(defparameter test-input-2 "Register A: 2024
+Register B: 0
+Register C: 0
+
+Program: 0,3,5,4,3,0")
 
 ;; If register C contains 9, the program 2,6 would set register B to 1.
 ;; If register A contains 10, the program 5,0,5,1,5,4 would output 0,1,2.
 ;; If register A contains 2024, the program 0,1,5,4,3,0 would output 4,2,5,6,7,7,7,7,3,1,0 and leave 0 in register A.
 ;; If register B contains 29, the program 1,7 would set register B to 26.
 ;; If register B contains 2024 and register C contains 43690, the program 4,0 would set register B to 44354.
+;;
+;; A: 64854237
+;; 2,4,
+;; 1,1,
+;; 7,5,
+;; 1,5,
+;; 4,0,
+;; 5,5,
+;; 0,3,
+;; 3,0
+;;
+;; BST 4
+;; BXL 1
+;; CDV 5
+;; BXL 5
+;; BXC 0
+;; OUT 5
+;; ADV 3
+;; JNZ 0
+;;
+;; while (a != 0) {
+;; b = a % 8       # BST 4
+;; b = b ^ 1       # BXL 1
+;; c = a // 1<<b   # CDV 5
+;; b = b ^ 5       # BXL 5
+;; b = b ^ c       # BXC 0
+;; output (b % 8)  # OUT 5
+;; a = a // 1<<3   # ADV 3
+;; }
 
 (defvar *a* 0)
 (defvar *b* 0)
@@ -18,10 +53,13 @@ Program: 0,1,5,4,3,0")
 (defvar *ip* 0)
 (defvar *output* nil)
 
+(defun instructions-as-array (instructions)
+  (make-array (length instructions) :element-type 'fixnum :initial-contents instructions))
+
 (defun read-program (input-file)
   (destructuring-bind (a b c &rest instructions)
       (ax:flatten (mapcar #'string-to-num-list (uiop:read-file-lines input-file)))
-    (values a b c (make-array (length instructions) :element-type 'fixnum :initial-contents instructions))))
+    (values a b c (instructions-as-array instructions))))
 
 (defun combo (cop)
   (ecase cop
@@ -65,24 +103,39 @@ Program: 0,1,5,4,3,0")
 
 (defvar op->fun '((0 . adv) (1 . bxl) (2 . bst) (3 . jnz) (4 . bxc) (5 . out) (6 . bdv) (7 . cdv)))
 
-(defun run-program (instructions)
+(defun run-program (instructions &optional ainc)
+  (when ainc
+    (setf *a* (+ *a* (* 8 ainc))))
   (let ((*output* nil))
-   (loop while (array-in-bounds-p instructions *ip*)
-         for func = (ax:assoc-value op->fun (aref instructions *ip*))
-         for operand = (aref instructions (1+ *ip*))
-         for res = (funcall func operand)
-         do (format t "~2,' d (FUNCALL ~a ~a)~%" *ip* func operand)
-         unless (and (eq func 'jnz) res)
-           do (incf *ip* 2))
+    (loop while (array-in-bounds-p instructions *ip*)
+          for func = (ax:assoc-value op->fun (aref instructions *ip*))
+          for operand = (aref instructions (1+ *ip*))
+          for res = (funcall func operand)
+          ;;do (format t "~2,' d (FUNCALL ~a ~a)~%" *ip* func operand)
+          unless (and (eq func 'jnz) res)
+            do (incf *ip* 2))
     (format nil "~{~a~^,~}" (reverse *output*))))
 
-;; guessed 1,7,7,0,5,7,3,2,7
-(defun day-17-part-1 (input-file)
+(defun day-17-part-1 (input-file &optional ainc)
   (multiple-value-bind (a b c instructions) (read-program input-file)
     (let ((*a* a) (*b* b) (*c* c) (*ip* 0) *output*)
-      (run-program instructions))))
+      (run-program instructions ainc))))
 
-(defun day-17-part-2 (input-file) (progn input-file -1))
+(defun first-n-match (arr1 arr2 n)
+  (loop for i from 0 below n always (= (aref arr1 i) (aref arr2 i))))
+
+(defun day-17-part-2 (input-file)
+  (let ((start 4399000320700)
+        (incs (circular! (list 32 303423 220833 32 524256))))
+   (multiple-value-bind (a b c instructions) (read-program input-file)
+     (loop for inc in incs
+           for ainc = start then (+ ainc inc)
+           for output-instructions = (instructions-as-array
+                                      (string-to-num-list
+                                       (let ((*a* a) (*b* b) (*c* c) (*ip* 0) *output*)
+                                         (run-program instructions ainc))))
+           when (equal instructions output-instructions)
+             return (format t "~a~%" ainc)))))
 
 (defun day-17 ()
   (let ((f (fetch-day-input-file 2024 17)))
