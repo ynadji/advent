c9efa6d1e2121b037317874cb1fa525b7c775509
Yacin Nadji <ynadji@gmail.com>
2024-12-17 18:20:07 -0500

    perf squeeze on day 7

--- 2024/07.lisp
+++ 2024/07.lisp
@@ -1,8 +1,5 @@
 (in-package :aoc2024)
 
-;(declaim (optimize (speed 3)))
-;; can't get helpful optimizations just yet
-
 (defparameter test-input "190: 10 19
 3267: 81 40 27
 83: 17 5
@@ -14,16 +11,18 @@
 292: 11 6 16 20")
 
 (defun fixnum-concat (x y)
-  (declare (type fixnum x y))
-  (+ (* x (expt 10 (num-digits y)))
-     y))
+  (declare (optimize (speed 3))
+           (type fixnum x y))
+  (the fixnum (+ (the fixnum (* x (the fixnum (expt 10 (the fixnum (num-digits y))))))
+                 y)))
 
 (defun solve-equation (test-value vals &optional (acc 0) (part 1))
-  (declare (type fixnum test-value acc part))
+  (declare (optimize (speed 3) (safety 0))
+           (type fixnum test-value acc part))
   (if (null vals)
       (= test-value acc)
-      (or (solve-equation test-value (rest vals) (the fixnum (+ acc (first vals))) part)
-          (solve-equation test-value (rest vals) (the fixnum (* acc (first vals))) part)
+      (or (solve-equation test-value (rest vals) (the fixnum (+ acc (the fixnum (first vals)))) part)
+          (solve-equation test-value (rest vals) (the fixnum (* acc (the fixnum (first vals)))) part)
           (and (= part 2) (solve-equation test-value (rest vals) (fixnum-concat acc (first vals)) part)))))
 
 (defun day-07% (input-file &optional (part 1))
