276d437c6528f960a15f0c9ed35aacc5059cb2ae
Yacin Nadji <ynadji@gmail.com>
2024-12-17 18:03:20 -0500

    day 14 speedups

--- 2024/14.lisp
+++ 2024/14.lisp
@@ -13,12 +13,14 @@ p=7,3 v=-1,2
 p=2,4 v=2,-3
 p=9,5 v=-3,-3")
 
-;;(declaim (optimize (speed 3)))
-
 (defun compute-robot-location (position-and-velocity max-x max-y n)
+  (declare (optimize (speed 3) (safety 0)))
   (destructuring-bind (x y dx dy) position-and-velocity
-    (cons (mod (+ x (* dx n)) max-x)
-          (mod (+ y (* dy n)) max-y))))
+    (declare (type fixnum x y dx dy max-x max-y n))
+    (let ((dxn (the fixnum (* dx n)))
+          (dyn (the fixnum (* dy n))))
+      (cons (mod (the fixnum (+ x dxn)) max-x)
+            (mod (the fixnum (+ y dyn)) max-y)))))
 
 (defun read-robots (input-file)
   (mapcar #'string-to-num-list (uiop:read-file-lines input-file)))
@@ -54,6 +56,16 @@ p=9,5 v=-3,-3")
     (loop for n from 1 for new-robots = (mapcar (lambda (pv) (compute-robot-location pv max-x max-y n)) robots)
           when (= num-robots (length (remove-duplicates new-robots :test #'equal)))
             return n)))
+
+(defun draw-robots (input-file n)
+  (let* ((robots (read-robots input-file))
+         (max-x 101)
+         (max-y 103)
+         (grid (make-array (list max-y max-x) :initial-element 0))
+         (positions (mapcar (lambda (pv) (compute-robot-location pv max-x max-y n)) robots)))
+    (loop for pos in positions do (incf (aref grid (cdr pos) (car pos))))
+    (with-open-file (stream #P"14-xmas-tree.txt" :direction :output :if-exists :supersede)
+     (print-grid grid :stream stream :char-color-alist '((0 . :black) (1 . :green))))))
 
 (defun day-14 ()
   (let ((f (fetch-day-input-file 2024 14)))
