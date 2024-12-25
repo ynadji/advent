5286de4b27cd8f417bd8a2d358fcabdfd05efbcb
Yacin Nadji <ynadji@gmail.com>
2024-12-25 14:13:34 -0500

    day 25 part 1

--- 2024/25.lisp
+++ 2024/25.lisp
@@ -1,6 +1,70 @@
 (in-package :aoc2024)
 
-(defun day-25-part-1 (input-file) (progn input-file -1))
+(defparameter test-input "#####
+.####
+.####
+.####
+.#.#.
+.#...
+.....
+
+#####
+##.##
+.#.##
+...##
+...#.
+...#.
+.....
+
+.....
+#....
+#....
+#...#
+#.#.#
+#.###
+#####
+
+.....
+.....
+#.#..
+###..
+###.#
+###.#
+#####
+
+.....
+.....
+.....
+#....
+#.#..
+#.#.#
+#####")
+
+(defun lock? (lines)
+  (every (lambda (c) (char= c #\#)) (first lines)))
+
+(defun read-key-lock (lines)
+  (let ((rotated (rotate-90-clockwise lines)))
+    (mapcar (lambda (line) (1- (count #\# line))) rotated)))
+
+(defun fits? (key lock)
+  (every (lambda (k l) (<= (+ k l) 5)) key lock))
+
+(defun parse-keys-and-locks (keys-or-locks)
+  (loop for kol in keys-or-locks
+        for lines = (str:split #\Newline kol)
+        if (lock? lines)
+          collect (read-key-lock lines) into locks
+        else
+          collect (read-key-lock lines) into keys
+        finally
+           (return (values keys locks))))
+
+(defun day-25-part-1 (input-file)
+  (let ((keys-or-locks (str:split (format nil "~%~%") (str:trim (uiop:read-file-string input-file)))))
+    (multiple-value-bind (keys locks) (parse-keys-and-locks keys-or-locks)
+      (loop for key in keys sum
+        (loop for lock in locks count (fits? key lock))))))
 
 (defun day-25-part-2 (input-file) (progn input-file -1))
 
