a6ef402bb4fd4c863796ddeeb91fa328bb45b8cf
Yacin Nadji <ynadji@gmail.com>
2025-01-17 15:12:24 -0500

    day 3

--- 2016/03.lisp
+++ 2016/03.lisp
@@ -1,8 +1,19 @@
 (in-package :aoc2016)
 
-(defun day-03-part-1 (input-file) (progn input-file -1))
+(defun triangle? (sides)
+  (destructuring-bind (a b c) sides
+    (and (> (+ a b) c)
+         (> (+ a c) b)
+         (> (+ b c) a))))
 
-(defun day-03-part-2 (input-file) (progn input-file -1))
+(defun day-03-part-1 (input-file)
+  (length (remove-if-not #'triangle? (mapcar #'string-to-num-list (uiop:read-file-lines input-file)))))
+
+(defun day-03-part-2 (input-file)
+  (labels ((read-triangle-columns (input-file)
+             (let ((lines (mapcar #'string-to-num-list (uiop:read-file-lines input-file))))
+               (partition (ax:flatten (transpose lines)) 3))))
+    (length (remove-if-not #'triangle? (read-triangle-columns input-file)))))
 
 (defun day-03 ()
   (let ((f (fetch-day-input-file 2016 3)))
