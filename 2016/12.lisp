958a96051be1291d18d125d7d81cd3d07ee49b76
Yacin Nadji <ynadji@gmail.com>
2025-01-30 09:46:03 -0500

    day 12

--- 2016/12.lisp
+++ 2016/12.lisp
@@ -1,10 +1,43 @@
 (in-package :aoc2016)
 
-(defun day-12-part-1 (input-file) (progn input-file -1))
+(defparameter test-input "cpy 41 a
+inc a
+inc a
+dec a
+jnz a 2
+dec a")
 
-(defun day-12-part-2 (input-file) (progn input-file -1))
+(defun parse-instruction-line (line)
+  (mapcar (lambda (x) (ax:if-let ((n (ignore-errors (parse-integer x)))) n (sintern x)))
+          (str:split #\Space line)))
+
+(defun run-assembunny (inst registers)
+  (let ((op (first inst))
+        (v1 (second inst)))
+    (ecase op
+      (|cpy| (if (numberp v1)
+                 (setf (gethash (third inst) registers) v1)
+                 (setf (gethash (third inst) registers) (gethash v1 registers))))
+      (|inc| (incf (gethash v1 registers)))
+      (|dec| (decf (gethash v1 registers)))
+      (|jnz| (values (not (zerop (if (numberp v1) v1 (gethash v1 registers))))
+                     (third inst))))))
+
+(defun day-12% (input-file &optional (c 0))
+  (let* ((registers (ax:alist-hash-table `((|a| . 0) (|b| . 0) (|c| . ,c) (|d| . 0))))
+         (instructions (mapcar #'parse-instruction-line (uiop:read-file-lines input-file)))
+         (instructions (make-array (length instructions) :initial-contents instructions))
+         (pc 0))
+    (loop while (array-in-bounds-p instructions pc)
+          for (r1 r2) = (multiple-value-list (run-assembunny (aref instructions pc) registers))
+          if (and r1 r2)
+            do (incf pc r2)
+          else
+            do (incf pc 1)
+          finally (return (values (gethash '|a| registers)
+                                  registers)))))
 
 (defun day-12 ()
   (let ((f (fetch-day-input-file 2016 12)))
-    (values (day-12-part-1 f)
-            (day-12-part-2 f))))
+    (values (day-12% f)
+            (day-12% f 1))))
