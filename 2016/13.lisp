4ef181407c44d7a84b6e962769417bc38677a9f4
Yacin Nadji <ynadji@gmail.com>
2025-02-07 10:04:41 -0500

    grid making funcs for day 13

--- 2016/13.lisp
+++ 2016/13.lisp
@@ -1,5 +1,15 @@
 (in-package :aoc2016)
 
+(defun wall? (x y fav-num)
+  (let ((num (+ fav-num (* x x) (* 3 x) (* 2 x y) y (* y y))))
+    (oddp (count #\1 (format nil "~b" num)))))
+
+(defun make-grid (fave-num)
+  (let ((grid (make-array (list fave-num fave-num) :element-type 'standard-char)))
+    (aops:each-index (i j)
+      (setf (aref grid i j) (if (wall? j i fave-num) #\# #\.)))
+    grid))
+
 (defun day-13-part-1 (input-file) (progn input-file -1))
 
 (defun day-13-part-2 (input-file) (progn input-file -1))
