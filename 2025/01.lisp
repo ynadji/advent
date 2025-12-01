1a783639de7900cab5df0baa052d8ab2256d1bab
Yacin Nadji <ynadji@gmail.com>
2025-12-01 17:28:46 -0500

    more efficient

--- 2025/01.lisp
+++ 2025/01.lisp
@@ -11,33 +11,23 @@ L99
 R14
 L82")
 
-(defun parse-dial-rotations (input-file)
-  (loop for line in (uiop:read-file-lines input-file)
-        collect (cons (char line 0) (parse-integer (subseq line 1)))))
-
-(defun day-01-part-1 (input-file)
-  (let ((rotations (parse-dial-rotations input-file))
-        (value 50))
-    (flet ((rotation-value (dir rotation)
-             (if (eq dir #\L)
-                 (- rotation)
-                 rotation)))
-     (loop for (dir . rotation) in rotations
-           do (setf value (mod (+ value (rotation-value dir rotation)) 100))
-           count (zerop value)))))
-
-(defun day-01-part-2 (input-file)
-  (let ((rotations (parse-dial-rotations input-file))
-        (value 50))
-    (loop for (dir . rotation) in rotations
-          sum (loop repeat rotation
-                    if (eq dir #\R)
-                      do (setf value (mod (1+ value) 100))
-                    else
-                      do (setf value (mod (1- value) 100))
-                    count (zerop value)))))
+(defun day-01% (input-file)
+  (flet ((parse-dial-rotations (input-file)
+           (loop for line in (uiop:read-file-lines input-file)
+                 collect (if (char= #\L (char line 0))
+                             (- (parse-integer (subseq line 1)))
+                             (parse-integer (subseq line 1))))))
+    (let ((dial 50) (part1 0) (part2 0))
+      (loop for rotation in (parse-dial-rotations input-file)
+            for new-dial = (+ dial rotation)
+            do (incf part2 (floor (abs new-dial) 100))
+               (when (and (plusp dial) (not (plusp new-dial)))
+                 (incf part2))
+               (setf dial (mod new-dial 100))
+               (when (zerop dial)
+                 (incf part1))
+            finally (return (values part1 part2))))))
 
 (defun day-01 ()
   (let ((f (fetch-day-input-file 2025 1)))
-    (values (day-01-part-1 f)
-            (day-01-part-2 f))))
+    (day-01% f)))
