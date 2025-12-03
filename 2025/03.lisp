0c75da701046fd5855bed23b37260456aab1d677
Yacin Nadji <ynadji@gmail.com>
2025-12-03 08:46:34 -0500

    refactor day 3

--- 2025/03.lisp
+++ 2025/03.lisp
@@ -16,21 +16,14 @@
            do (setf max x index i)
          finally (return (values max index)))))
 
-(defun day-03-part-1 (input-file)
+(defun day-03% (input-file num-digits)
   (loop for bank in (parse-battery-banks input-file)
-        for (max1 index1) = (multiple-value-list (find-max-within bank))
-        for (max2 index2) = (multiple-value-list (find-max-within bank (1+ index1) (length bank)))
-        sum (+ (* 10 max1) max2)))
-
-(defun day-03-part-2 (input-file)
-  (declare (optimize debug))
-  (loop for bank in (parse-battery-banks input-file)
-        sum (loop for n from 11 downto 0 with start-index = 0
+        sum (loop for n from (1- num-digits) downto 0 with start-index = 0
                       for (max-n index-n) = (multiple-value-list (find-max-within bank start-index (- (length bank) n)))
                       do (setf start-index (1+ index-n))
                       sum (* max-n (expt 10 n)))))
 
 (defun day-03 ()
   (let ((f (fetch-day-input-file 2025 3)))
-    (values (day-03-part-1 f)
-            (day-03-part-2 f))))
+    (values (day-03% f 2)
+            (day-03% f 12))))
