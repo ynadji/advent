341fbf78091a6845be85e46b9e43b3d7ab751511
Yacin Nadji <ynadji@gmail.com>
2025-12-05 11:47:11 -0500

    much faster day 2

--- 2025/02.lisp
+++ 2025/02.lisp
@@ -5,39 +5,41 @@
 824824821-824824827,2121212118-2121212124")
 
 (defun parse-id-ranges (input-file)
-  (mapcar (lambda (x) (mapcar #'parse-integer (str:split #\- x))) (str:split #\, (uiop:read-file-string input-file))))
+  (mapcar (lambda (x) (mapcar #'parse-integer (str:split #\- x)))
+          (str:split #\, (uiop:read-file-string input-file))))
 
 (defun invalid-id-p1? (id)
   (let ((num-digits-id (num-digits id)))
     (when (evenp num-digits-id)
       (zerop (mod id (1+ (expt 10 (/ (num-digits id) 2))))))))
 
-(defun one-ohs (length)
-  (let ((one-ohs (loop for x from length downto 1 by 2 sum (expt 10 (1- x)))))
-    (if (zerop (mod one-ohs 10))
-        (/ one-ohs 10)
-        one-ohs)))
+(defun generate-invalids ()
+  (flet ((repeat-int (s k)
+           (loop repeat (1- s) with repeated = k with length = (num-digits k)
+                 do (setf repeated (+ (* repeated (expt 10 length)) k))
+                 finally (return repeated))))
+    (let (invalids)
+      (loop for n from 1 to 99999
+            do (let* ((length (num-digits n)))
+                 (when (<= 1 length 5)
+                   ;; k from 2 up to floor(10/len), inclusive
+                   (loop for k from 2 to (floor 10 length)
+                         for s = (repeat-int k n)
+                         do (push s invalids)))))
+      (remove-duplicates invalids))))
 
-(defun invalid-id-p2? (id)
-  (let ((num-digits-id (num-digits id)))
-    (when (> num-digits-id 1)
-     (or (invalid-id-p1? id) ;; 10*1
-         ;; 101...
-         (when (and (zerop (mod num-digits-id 2)) (> num-digits-id 3))
-           (zerop (mod id (one-ohs num-digits-id))))
-         (when (= num-digits-id 9)
-           (zerop (mod id 1001001)))
-         ;; 111...
-         (zerop (mod id (/ (1- (expt 10 num-digits-id)) 9)))))))
-
-(defun day-02% (input-file &optional (invalid-id? #'invalid-id-p1?))
+(defun range-check-day-02 (input-file invalids)
+  (declare (optimize speed))
   (let ((id-ranges (parse-id-ranges input-file)))
-    (loop for (start end) in id-ranges
-          sum (loop for id from start upto end
-                    when (funcall invalid-id? id)
-                      sum id))))
+    (loop for invalid fixnum in invalids
+          when (loop for (start end) in id-ranges
+                       thereis (<= (the fixnum start) invalid (the fixnum end)))
+            sum invalid into p2 fixnum and
+          when (invalid-id-p1? invalid)
+            sum invalid into p1 fixnum
+          finally (return (values p1 p2)))))
 
 (defun day-02 ()
-  (let ((f (fetch-day-input-file 2025 2)))
-    (values (day-02% f)
-            (day-02% f #'invalid-id-p2?))))
+  (let ((f (fetch-day-input-file 2025 2))
+        (invalids (generate-invalids)))
+    (range-check-day-02 f invalids)))
