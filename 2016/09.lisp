580b54b0da025fe47cce928749eb944bc2580b94
Yacin Nadji <ynadji@gmail.com>
2026-04-23 11:25:01 -0400

    day 9, 2016

--- 2016/09.lisp
+++ 2016/09.lisp
@@ -7,34 +7,28 @@ A(2x2)BCD(2x2)EFG
 (6x1)(1x3)A
 X(8x2)(3x3)ABCY")
 
-;; works with the test cases but wrong answer :(
-(defun decompressed-length (s)
-  (labels ((parse-marker (s start)
-             (cl-ppcre:register-groups-bind ((#'parse-integer x y)) ("\\((\\d+)x(\\d+)\\)" s :start start)
-               (values x y))))
-    (loop for c across s with total-length = 0 with in-marker? = nil
-          with marker-length = 0 with maybe-add = 0 for i from 0 do
-            (cond ((and (char= #\( c) (zerop marker-length))
-                   (setf in-marker? t)
-                   (multiple-value-bind (num-chars num-repeats) (parse-marker s i)
-                     (setf marker-length num-chars)
-                     (setf maybe-add (- (* num-chars num-repeats) num-chars))))
-                  (in-marker? (when (char= #\) c)
-                                (setf in-marker? nil)))
-                  ((and (plusp marker-length) (char/= #\) c))
-                   (decf marker-length)
-                   (incf total-length)
-                   (when (zerop marker-length)
-                     (incf total-length maybe-add)
-                     (setf maybe-add 0)))
-                  (t (incf total-length)))
-          finally (return total-length))))
+(defun decompressed-length (s &key (start 0) (end (length s)) part2?)
+  (loop with i = start with total = 0 while (< i end) do
+    (let ((c (char s i)))
+      (if (char= c #\()
+          (multiple-value-bind (ms me rs re) (cl-ppcre:scan "\\((\\d+)x(\\d+)\\)" s :start i)
+            (declare (ignore ms))
+            (let ((n (parse-integer s :start (aref rs 0) :end (aref re 0)))
+                  (m (parse-integer s :start (aref rs 1) :end (aref re 1))))
+              (incf total (* m (if part2?
+                                   (decompressed-length s :start me :end (+ me n) :part2? t)
+                                   n)))
+              (setf i (+ me n))))
+          (progn (incf total) (incf i))))
+        finally (return total)))
 
 (defun day-09-part-1 (input-file)
   (let ((s (uiop:read-file-string input-file)))
-    (decompressed-length (str:trim s))))
+    (decompressed-length (remove #\Newline s))))
 
-(defun day-09-part-2 (input-file) (progn input-file -1))
+(defun day-09-part-2 (input-file)
+  (let ((s (uiop:read-file-string input-file)))
+    (decompressed-length (remove #\Newline s) :part2? t)))
 
 (defun day-09 ()
   (let ((f (fetch-day-input-file 2016 9)))
