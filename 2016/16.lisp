9cb96f48d26e427090e3373c0264e48c4e225d88
Yacin Nadji <ynadji@gmail.com>
2026-05-06 17:45:19 -0400

    day 16

--- 2016/16.lisp
+++ 2016/16.lisp
@@ -1,10 +1,28 @@
 (in-package :aoc2016)
 
-(defun day-16-part-1 (input-file) (progn input-file -1))
+(defun fill-disk-step (a)
+  (reverse (map 'string (lambda (c) (if (char= c #\0) #\1 #\0)) a)))
 
-(defun day-16-part-2 (input-file) (progn input-file -1))
+(defun fill-disk-until (a limit)
+  (if (>= (length a) limit)
+      (subseq a 0 limit)
+      (fill-disk-until (concatenate 'string a "0" (fill-disk-step a)) limit)))
+
+(defun disk-checksum (a)
+  (flet ((checksum% (a)
+           (coerce (loop for (x y) on (coerce a 'list) by #'cddr
+                         collect (if (char= x y) #\1 #\0))
+                   'string)))
+    (let ((chksum (checksum% a)))
+      (if (oddp (length chksum))
+          chksum
+          (disk-checksum chksum)))))
+
+(defun day-16% (input-file)
+  (let ((a (str:trim (uiop:read-file-string input-file))))
+    (values (disk-checksum (fill-disk-until a 272))
+            (disk-checksum (fill-disk-until a 35651584)))))
 
 (defun day-16 ()
   (let ((f (fetch-day-input-file 2016 16)))
-    (values (day-16-part-1 f)
-            (day-16-part-2 f))))
+    (day-16% f)))
