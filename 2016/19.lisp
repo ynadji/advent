dcb021406537565175bae82cc520728d94239355
Yacin Nadji <ynadji@gmail.com>
2026-05-08 09:05:42 -0400

    day 19

--- 2016/19.lisp
+++ 2016/19.lisp
@@ -1,8 +1,19 @@
 (in-package :aoc2016)
 
-(defun day-19-part-1 (input-file) (progn input-file -1))
+(defun josephus-safe (n)
+  (let ((larger-pow-2 (expt 2 (ceiling (log n) (log 2)))))
+    (1+ (mod (ash n 1) larger-pow-2))))
 
-(defun day-19-part-2 (input-file) (progn input-file -1))
+(defun doophus-safe (n)
+  (loop with x = 1 while (< (* x 3) n)
+        do (setf x (* x 3))
+        finally (return (- n x))))
+
+(defun day-19-part-1 (input-file)
+  (josephus-safe (parse-integer (uiop:read-file-string input-file))))
+
+(defun day-19-part-2 (input-file)
+  (doophus-safe (parse-integer (uiop:read-file-string input-file))))
 
 (defun day-19 ()
   (let ((f (fetch-day-input-file 2016 19)))
