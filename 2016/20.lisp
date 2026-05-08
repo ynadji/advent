be97c57d50e7b07539a2c0221faa619cdc62da43
Yacin Nadji <ynadji@gmail.com>
2026-05-07 21:03:23 -0400

    day 20

--- 2016/20.lisp
+++ 2016/20.lisp
@@ -1,10 +1,32 @@
 (in-package :aoc2016)
 
-(defun day-20-part-1 (input-file) (progn input-file -1))
+(defparameter test-input "5-8
+0-2
+4-7")
 
-(defun day-20-part-2 (input-file) (progn input-file -1))
+(defun parse-ips (input-file)
+  (loop for line in (uiop:read-file-lines input-file)
+        collect (mapcar #'parse-integer (str:split #\- line))))
+
+(defun day-20% (blocked-ip-ranges)
+  (let* ((sorted-ip-ranges (sort blocked-ip-ranges #'< :key #'first))
+         (prev-start (first (first sorted-ip-ranges)))
+         (prev-end (second (first sorted-ip-ranges)))
+         (blocked-ips 0)
+         (first-allowed))
+    (loop for (start end) in (rest sorted-ip-ranges)
+          if (or (= (1+ prev-end) start)
+                 (<= prev-start start prev-end))
+            do (setf prev-end (max end prev-end))
+          else
+            do (unless first-allowed
+                 (setf first-allowed (1+ prev-end)))
+               (incf blocked-ips (1+ (- prev-end prev-start)))
+               (setf prev-start start prev-end end)
+          finally (incf blocked-ips (1+ (- (max end prev-end) (min start prev-start)))))
+    (values first-allowed
+            (- (expt 2 32) blocked-ips))))
 
 (defun day-20 ()
   (let ((f (fetch-day-input-file 2016 20)))
-    (values (day-20-part-1 f)
-            (day-20-part-2 f))))
+    (day-20% (parse-ips f))))
