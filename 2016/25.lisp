1bea0d27b62324b545dcd43b8b1bc90f62f38261
Yacin Nadji <ynadji@gmail.com>
2026-05-13 22:36:12 -0400

    day 25

--- 2016/25.lisp
+++ 2016/25.lisp
@@ -1,6 +1,8 @@
 (in-package :aoc2016)
 
-(defun day-25-part-1 (input-file) (progn input-file -1))
+(defun day-25-part-1 (input-file)
+  (loop for a from 0 for output = (day-12% input-file :a a)
+        when output do (return a)))
 
 (defun day-25-part-2 (input-file) (progn input-file -1))
 
